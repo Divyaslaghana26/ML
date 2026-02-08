aadf43545a7b6dcf7c871161ef01f316031c9776
Divyaslaghana26 <bl.sc.u4aie24041@bl.students.amrita.edu>
2026-02-08 21:07:07 +0530

    Add files via upload

---
+++ LAB_04/output.py
@@ -0,0 +1,14 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+==== RESTART: C:/Users/Divya Rangumudri/Downloads/SEM-4/ML/LAB4/lab4codes.py ===
+training results
+[[1086   13    0]
+ [  12 1090    2]
+ [   0    0 1154]]
+0.9919513688435465 0.9919571045576407 0.9919538454940127
+testing_result
+[[250  21   0]
+ [ 16 263   3]
+ [  0   0 287]]
+0.9522359165797656 0.9523809523809523 0.952269306046887
